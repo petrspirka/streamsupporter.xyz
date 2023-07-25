bffebff880e9502ef7541a1271b366ca07e1303b
IndigoEU <dragioeu@gmail.com>
2023-07-25 13:40:32 +0200

    Fixed more bugs and updated database

---
+++ Migrations/20230725063231_Adjusted CounterModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewStreamSupporter.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustedCounterModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "TriggeredByDonations",
+                table: "CounterModel",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TriggeredByDonations",
+                table: "CounterModel");
+        }
+    }
+}
