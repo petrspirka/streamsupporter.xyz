de17e7e32d599bd392f74d1f8d1be1c4eefad701
IndigoEU <dragioeu@gmail.com>
2023-08-03 12:56:01 +0200

    Changed AlertDuration datatype

--- Migrations/20230803105433_Test.Designer.cs
+++ Migrations/20230803105433_Test.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NewStreamSupporter.Data;
 
@@ -10,9 +11,11 @@ using NewStreamSupporter.Data;
 namespace NewStreamSupporter.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230803105433_Test")]
+    partial class Test
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.5");
---
+++ Migrations/20230803105433_Test.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewStreamSupporter.Migrations
+{
+    /// <inheritdoc />
+    public partial class Test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AlertDuration",
+                table: "Alerts",
+                type: "decimal(5, 2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(9, 2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AlertDuration",
+                table: "Alerts",
+                type: "decimal(9, 2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5, 2)");
+        }
+    }
+}
